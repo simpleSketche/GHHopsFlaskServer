518000071da609b99c566c742a628adf1146321a
yangyankunGDS <yangyankun.work@gmail.com>
2022-10-06 09:34:14 -0400

    update

--- src/run_gh_file.py
+++ src/run_gh_file.py
@@ -1,9 +1,7 @@
 #tested with python3.7
 from pathlib import Path
-from tokenize import String
 import rhinoinside
 import clr
-import sys
 sysdir = Path(r"C:\Program Files\Rhino 7\System")
 plugdir = Path(sysdir, "..", "Plug-ins").resolve()
 rhinoinside.load()
@@ -16,12 +14,10 @@ clr.AddReference(GrasshopperDll)
 clr.AddReference(GH_IODll)
 clr.AddReference(GH_UtilDll)
 # Set up ready, now do the actual Rhino usage
-import System
 import Rhino
-from Grasshopper.Kernel import GH_Document, GH_SolutionMode
+from Grasshopper.Kernel import GH_Document
 from GH_IO.Serialization import GH_Archive
 from Grasshopper.Kernel.Data import GH_Path
-from Grasshopper.Kernel.Types import GH_String
 from Grasshopper.Kernel import IGH_Param
 
 
@@ -90,7 +86,7 @@ def gh_make_sphere(radius):
             mesh = result.Value
             area = Rhino.Geometry.AreaMassProperties.Compute(mesh).Area
             output["result"] = area
-            print(type(result))
+            print(area)
 
             # the method below displays all properties / methods of IGH_Param
             # print(dir(param))
